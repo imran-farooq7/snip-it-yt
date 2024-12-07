716956585b0b1bc55ff321d3bcafea5b4d7a2a63
Imran Farooq <imran_farooq7@hotmail.com>
2024-12-07 06:55:50 +0500

    snippets route updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,6 @@ export default function RootLayout({
 					}}
 				>
 					<Navbar />
-					<TechBar />
 
 					{children}
 					<Toaster />
--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default async function Home() {
 	const { data: snippets } = await getAllSnippets();
 	return (
 		<>
+			<TechBar />
 			<SearchBar snippets={snippets} />
 		</>
 	);
